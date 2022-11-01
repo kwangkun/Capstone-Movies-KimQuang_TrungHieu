71de5e0fcb590f0baf38bf79ad3e170bcc39510c
trunghieu <hoanghieu30092000@gmail.com>
2022-11-01 17:37:09 +0700

    update

--- movie/src/components/layouts/MainLayout.jsx
+++ movie/src/components/layouts/MainLayout.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import styled from 'styled-components';
+import Header from '../molecules/HomeHeader';
+import Footer from '../molecules/HomeFooter';
 
 const MainLayout = () => {
     return (
-        <div>
+        <Container className='Main-layout'>
+            <Header />
             <main className='main-outlet'>
                 <Outlet />
             </main>
-        </div>
+            <Footer />
+        </Container>
     )
 }
 
 export default MainLayout
+
+
+const Container = styled.div`
+  &.MainLayout {
+    .Header {
+      position: sticky;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 101;
+      background-color: #2b2b31;
+      box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
+        rgba(0, 0, 0, 0.23) 0px 6px 6px;
+    }
+
+    .main-content {
+      flex: 1;
+    }
+
+    .Footer {
+      background-color: #2b2b31;
+    }
+  }
+`;
