23ece6552eb1711d9363a581ec4abbaf986a4261
kwangkun <kimquang2002@gmail.com>
2022-11-08 00:39:07 +0700

    update reducer

--- movie/src/services/quanLyNguoiDungService.js
+++ movie/src/services/quanLyNguoiDungService.js
@@ -6,5 +6,23 @@ export const quanLyNguoiDungService = {
     },
     register: (thongTinDangKy) => {
         return api.post('QuanLyNguoiDung/DangKy', thongTinDangKy)
+    },
+    information: () => {
+        return api.post("QuanLyNguoiDung/ThongTinTaiKhoan");
+    },
+    getUserList: (keyword) => {
+        return api.get(`QuanLyNguoiDung/LayDanhSachNguoiDung?MaNhom=gp00&${keyword}`)
+    },
+    addUser: (user) => {
+        return api.post(`QuanLyNguoiDung/ThemNguoiDung`, user)
+    },
+    deleteUser: (user) => {
+        return api.delete(`QuanLyNguoiDung/XoaNguoiDung?TaiKhoan=${user}`)
+    },
+    getUserInfo: (user) => {
+        return api.post(`/api/QuanLyNguoiDung/LayThongTinNguoiDung?taiKhoan=${user}`)
+    },
+    updateUserInfo: (user) => {
+        return api.post(`/api/QuanLyNguoiDung/CapNhatThongTinNguoiDung`, user)
     }
 }
